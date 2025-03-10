3974d234d61d5347a30073c259d8cf4972ae8404
ProgramLogic <wredthecoder@gmail.com>
2025-03-10 13:17:13 -0600

    added

--- script2.js
+++ script2.js
@@ -1,25 +1,37 @@
-    // Get the modal
-    var modal = document.getElementById("myModal");
+// Get the modal and button elements
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
 
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
+// Open the modal when the "Make Recipe" button is clicked
+btn.onclick = function() {
+  modal.style.display = "block";
+}
 
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+// Close the modal when the "X" is clicked
+span.onclick = function() {
+  modal.style.display = "none";
+}
 
-    // When the user clicks on the button, open the modal
-    btn.onclick = function() {
-      modal.style.display = "block";
-    }
+// Close the modal if the user clicks outside of the modal content
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-      modal.style.display = "none";
-    }
+// Handle form submission
+document.getElementById("recipeForm").onsubmit = function(e) {
+  e.preventDefault(); // Prevent page reload
+  
+  var title = document.getElementById("recipeTitle").value;
+  var ingredients = document.getElementById("ingredients").value;
+  var instructions = document.getElementById("instructions").value;
 
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    }
+  console.log("Recipe Title: " + title);
+  console.log("Ingredients: " + ingredients);
+  console.log("Instructions: " + instructions);
+
+  // You can store the recipe or display it somewhere
+  modal.style.display = "none"; // Close modal after submission
+}
