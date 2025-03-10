822b990bd5fdaadd09260978bfd0af446726cbf9
ProgramLogic <wredthecoder@gmail.com>
2025-03-10 11:25:40 -0600

    better btns

---
+++ script2.js
@@ -0,0 +1,13 @@
+function createRecipe() {
+    alert("Create a new recipe!");
+    
+  }
+  
+  function viewRecipes() {
+    alert("Here are your created recipes!");
+  }
+  
+  function deleteRecipe() {
+    alert("Delete a recipe!");
+  }
+  
